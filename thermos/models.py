b3fd1e54fa0fbbd408b0597b22678e182389d2ce
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-27 16:31:34 +0200

    Added SqlAlchemy

---
+++ thermos/models.py
@@ -0,0 +1,24 @@
+
+from datetime import datetime
+from thermos import db
+
+# Database entities used to generate tables in SQL Alchemy
+
+class Bookmark(db.Model)
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    description = db.Column(db.String(300))
+
+    # Clear printing and logging of values
+    def __repr__(self):
+        return "<Bookmark '{}': '{}'>".format(self.description, self.url)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
