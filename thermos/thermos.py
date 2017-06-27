dc4d8a7e163f18520b956a951540d81473915c84
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-27 14:10:34 +0200

    Added custom error pages

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -12,13 +12,24 @@ def index():
     """"
     Renders the default index template
     """
-    return render_template('index.html')
+    return ender_template('index.html')
 
 
 @app.route('/add')
 def add():
     return render_template("add.html")
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
