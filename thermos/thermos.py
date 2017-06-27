a3ea833a2d68c3aaa0382baf9d521c7becdb95d0
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-27 14:00:37 +0200

    Added template inheritance

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,14 +1,23 @@
 
-# Main entry point of web app
+# Main entry point of web app used to start up Flask
 
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+
+@app.route('/')
+@app.route('/index')
 def index():
+    """"
+    Renders the default index template
+    """
     return render_template('index.html')
+
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
