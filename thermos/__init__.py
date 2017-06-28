9a288193010d1319e93d46d7f06ebb7d64065331
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-28 16:21:15 +0200

    Added doc

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -24,7 +24,7 @@ db = SQLAlchemy(app)
 # Configure authentication
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
-login_manager.login_view = "login"
+login_manager.login_view = "auth.login"
 login_manager.init_app(app)
 
 # Enable debugtoolbar
--- thermos/auth/__init__.py
+++ thermos/auth/__init__.py
@@ -1,5 +1,7 @@
 
-# Create blueprint object of current module (auth)
+# Create blueprint object of current module (authentication functionality)
+# Blueprint of how to construct authentication modules
+# Blueprint used to break up growing application into modules
 
 from flask import Blueprint
 
