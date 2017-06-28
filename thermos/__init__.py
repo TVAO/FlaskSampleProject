4412601f6e22dbe22735017ca7084a77454c0bc6
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-28 11:30:23 +0200

    Removed deprecated god module (thermos file)

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -12,15 +12,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 app.config["SECRET_KEY"] = b'\x12\x1d\x9c\xfd\x8e\xd5\r\xdb\x81\x99t\x98\x01.2\xe2Ra\x07\x14\x92.\tD'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 
 import models
 import views
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
 
 
