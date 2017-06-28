76a6f34792d3f3e993c2caa2f263313106eaf6b6
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-28 14:01:21 +0200

    Added sign up form

--- thermos/forms.py
+++ thermos/forms.py
@@ -4,7 +4,9 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.fields.html5 import URLField
-from wtforms.validators import DataRequired, url
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, \
+    url, ValidationError
+from models import User
 
 
 class BookmarkForm(FlaskForm):
@@ -36,3 +38,26 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[
+                               DataRequired(), Length(3, 80),
+                               Regexp('^[A-Z-z0-9_]{3,}$',
+                                      message='Usernames consists of numbers, letters, and underscores')
+                           ])
+    password = PasswordField('Password',
+                             validators=[
+                                 DataRequired(),
+                                 EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 120), Email()])
+
+    def validate_email(self, email_field): # Generate error on email field
+        if User.query.filter_by(email=email_field.data).first():
+            raise ValidationError('There already is a user with this email address.')
+
+    def validate_username(self, username_field): # Generate error on username field
+        if User.query.filter_by(username=username_field.data).first():
+            raise ValidationError('This username is already taken.')
