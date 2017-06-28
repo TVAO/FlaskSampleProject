61b0d53cb437ee80206942e8b9184225c5d5df1f
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-28 15:14:09 +0200

    Added seed method

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 # Manage database setup and run server (admin interface)
 
 from thermos import app, db
-from thermos.models import User, Bookmark # Need to be there for migration
+from thermos.models import User, Bookmark, Tag # Need to be there for migration
 from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 
@@ -13,11 +13,28 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def initdb():
-    db.create_all()
-    db.session.add(User(username="tvao", email="tvao@itu.dk", password="test"))
+def insert_data():
+    tvao = User(username="tvao", email="tvao@example.com", password="test")
+    db.session.add(reindert)
+
+    def add_bookmark(url, description, tags):
+        db.session.add(Bookmark(url=url, description=description, user=tvao,
+                                tags=tags))
+
+    for name in ["python", "flask", "webdev", "programming", "training", "news", "orm", "databases", "emacs", "gtd", "django"]:
+        db.session.add(Tag(name=name))
     db.session.commit()
-    print('Initialized the database')
+
+    add_bookmark("http://www.pluralsight.com", "Pluralsight. Hardcore developer training.", "training,programming,python,flask,webdev")
+    add_bookmark("http://www.python.org", "Python - my favorite language", "python")
+    add_bookmark("http://flask.pocoo.org", "Flask: Web development one drop at a time.", "python,flask,webdev")
+    add_bookmark("http://www.reddit.com", "Reddit. Frontpage of the internet", "news,coolstuff,fun")
+    add_bookmark("http://www.sqlalchemyorg", "Nice ORM framework", "python,orm,databases")
+
+    arjen = User(username="test", email="test@example.com", password="test")
+    db.session.add(arjen)
+    db.session.commit()
+    print 'Initialized the database'
 
 
 @manager.command
