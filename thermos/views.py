123f2c9a59d13041838625e920427a0e74a930cb
Thor Valentin Aakjær Nielsen Olesen <tvao@itu.dk>
2017-06-28 14:07:20 +0200

    Added signup action

--- thermos/views.py
+++ thermos/views.py
@@ -4,11 +4,11 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, login_user, current_user, logout_user
 from thermos import app, db, login_manager
-from forms import BookmarkForm, LoginForm
+from forms import BookmarkForm, LoginForm, SignupForm
 from models import User, Bookmark
 # New Python 3 import
 #from . import app, db, login_manager
-#from .forms import BookmarkForm, LoginForm
+#from .forms import BookmarkForm, LoginForm, SignupForm
 #from .models import User, Bookmark
 
 
@@ -69,6 +69,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route("/signup". methods=['GET, POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Welcome, {}! Please login.'.format(user.username))
+        return redirect(url_for('login'))
+    return render_template("signup.html", form=form)
 
 
 @app.errorhandler(404)
